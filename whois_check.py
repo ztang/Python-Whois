1d70b996de044369b32cba012c847603476676d0
ztang <root@tzq.me>
2019-07-29 17:09:20 +0800

    fix a security issue

--- whois_check.py
+++ whois_check.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import subprocess as sp
+import re
 
 def get_whois(domain_name):
     try:
-        whois_info = sp.check_output('whois %s' % domain_name, shell=True)
-        return whois_info
-    except sp.CalledProcessError, e:
+        domain_name_match = re.match(r'[a-zA-Z0-9][-a-zA-Z0-9]{0,62}\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62}', domain_name)
+        domain_name_match_www = re.match(r'[a-zA-Z0-9][-a-zA-Z0-9]{0,4}\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62}\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62}', domain_name)
+        if domain_name_match == None:
+            return 'Input Error \n Please input correct domain name!'
+        else:
+            if domain_name_match_www == None:
+                domain_name = domain_name_match.group()
+            else:
+                dnw = domain_name_match_www.group()
+                domain_name = dnw.split('.')[1]+ '.' + dnw.split('.')[2]
+            whois_info = sp.check_output('whois %s' % domain_name, shell=True)
+            return whois_info
+    except sp.CalledProcessError as e:
         return e.output
 
 
@@ -15,8 +26,10 @@ if __name__ == '__main__':
     dn = 'tang.com'
     dn_whois = get_whois(dn)
     dn_error = get_whois('ttt')
-    print dn_error
+    print(dn_error)
     #print dn_whois
     #print type(dn_whois)
-    dn_whois = dn_whois.replace('\n', '<br>')
+    dn_whois_check = dn_whois.decode('utf-8')
+    if re.match(r'\n', dn_whois_check) != None:
+        dn_whois = dn_whois.encode().replace('\n', '<br>')
     #print dn_whois
